5b4bc549f72221b1f1df6bf39ded2702c5250594
Amarnath S <amarnath.s@microfocus.com>
2022-03-21 10:25:14 +0530

    nothing here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> scs  .
+          Edit <code>src/App.js</code> scs  Test1.
         </p>
         <a
           className="App-link"
